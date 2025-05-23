e034b79ab05fd3264ee74c18ab348b98eee667c3
fox-programmer <foxyfnaf.93yt@gmail.com>
2025-05-23 21:17:46 +0200

    Crete basic UI (features are not available yet)

--- app/src/main/java/com/example/foxnotemini/MainActivity.kt
+++ app/src/main/java/com/example/foxnotemini/MainActivity.kt
@@ -1,26 +1,29 @@
 package com.example.foxnotemini
 
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(1)
+
+        )
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val button = findViewById<Button>(R.id.addNoteButton)
-        button.setOnClickListener {
-            setContentView(R.layout.view_notes)
         }
     }
 }
