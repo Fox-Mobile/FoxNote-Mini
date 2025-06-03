006badb143c2a14e93efbee6221f7965edff7a47
fox-programmer <foxyfnaf.93yt@gmail.com>
2025-06-03 18:06:10 +0200

    hotFix

--- app/src/main/java/com/example/foxnotemini/MainActivity.kt
+++ app/src/main/java/com/example/foxnotemini/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.foxnotemini
 
 import android.os.Bundle
-import android.widget.Toast
-import android.widget.Toolbar
-import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
+    private val overviewFragment = OverviewFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
@@ -24,6 +22,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, overviewFragment)
+            commitNow()
         }
     }
 }
