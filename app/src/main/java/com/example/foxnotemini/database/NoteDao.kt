28ac85fd9e47eceeb5cf9d3bf978b3a8f948f6d0
fox-programmer <foxyfnaf.93yt@gmail.com>
2025-06-19 12:51:37 +0200

    deleting note feature working

--- app/src/main/java/com/example/foxnotemini/database/NoteDao.kt
+++ app/src/main/java/com/example/foxnotemini/database/NoteDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
@@ -14,6 +15,6 @@ interface NoteDao {
     suspend fun deleteNote(note: Note)
 
     @Query("SELECT * FROM note ORDER BY dateTime DESC")
-    suspend fun getNotes(): List<Note>
+    fun getNotes(): Flow<List<Note>>
 
 }
